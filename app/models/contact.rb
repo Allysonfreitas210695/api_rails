02c94a7baa15b878366ba8731bc374bee9b168e6
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-02-02 21:25:14 -0300

    Usando POST com associacoes (belongs_to)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,19 +1,19 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind, optional: true
 
-  def autor
-    'Allyson bruno'
-  end
-
-  def kind_description
-    self.kind.description
-  end
-
-  def as_json(options={})
-    super(
-      root: true,
-      methods: [:kind_description, :autor],
-      # include: { kind: { only: :description }}
-    )
-  end
+  # def autor
+  #   'Allyson bruno'
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :autor],
+  #     # include: { kind: { only: :description }}
+  #   )
+  # end
 end
